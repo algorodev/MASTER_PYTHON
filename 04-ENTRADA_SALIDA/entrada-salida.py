4345543cdcd785a1b12f2e8a75a6a4dc001d3623
algoroloro <algoro9697@gmail.com>
2021-01-28 19:18:55 +0100

    Entrada y Salida de datos por consola en PYTHON

---
+++ 04-ENTRADA_SALIDA/entrada-salida.py
@@ -0,0 +1,6 @@
+# Entrada de datos que nos proporciona el usuario
+nombre = input("¿Cual es tu nombre?: ")
+edad = input("¿Cuantos años tienes?: ")
+
+# Salida de datos al usuario
+print(f"Encantado de conocerte {nombre}, veo que tienes {edad} años!!")
