7791de54d7f671b88f0570da45ba0b201f44332c
algoroloro <algoro9697@gmail.com>
2021-01-28 18:52:20 +0100

    Curiosidades de los Strings en PYTHON

---
+++ 02-VARIABLES_Y_TIPOS/curiosidades.py
@@ -0,0 +1,23 @@
+# Se permiten los pisos en los nombres de variables pero no los guiones
+hola_mundo = "Hola Mundo"
+
+# Mostrar un texto entre comillas
+texto = "Hola que tal soy \"Alejandro\""
+texto2 = "Hola que tal soy 'Alejandro'"
+texto3 = 'Hola que tal soy "Alejandro"'
+print(texto)
+print(texto2)
+print(texto3)
+
+# Saltos de línea
+texto_con_salto = "Hola, ¿que tal? \n Bien, ¿y tú?"
+print(texto_con_salto)
+
+# Tabulación
+texto_con_tab = "Hola, ¿que tal? \t Bien, ¿y tú?"
+print(texto_con_tab)
+
+# Borrar la primera variable
+hola = "Hola"
+que_tal = "¿Qué tal?"
+print(hola + "\r" + que_tal)
