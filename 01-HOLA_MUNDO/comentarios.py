c78243646f3fbb377acac2d741dfebac901628e5
algoroloro <algoro9697@gmail.com>
2021-01-28 17:57:48 +0100

    Comentarios en PYTHON

---
+++ 01-HOLA_MUNDO/comentarios.py
@@ -0,0 +1,9 @@
+# ESTO ME MOSTRARA POR CONSOLAR EL SIGUIENTE TEXTO
+print('Hola')
+print('Mundo')
+print('!!!!!')
+
+"""
+ESTO ES UN COMENTARIO MULTILINEA
+print('Esto no se va a mostrar en pantalla')
+"""
