a802fcd36b6cc97abd37a534eab5f83eb032416d
algoroloro <algoro9697@gmail.com>
2021-01-28 17:58:07 +0100

    Variables y Concatenaciones en PYTHON

---
+++ 02-VARIABLES_Y_TIPOS/variables.py
@@ -0,0 +1,17 @@
+# CREANDO VARIABLES Y ASIGNANDOLES UN VALOR
+texto = "Esto es una variable de tipo texto"
+print(texto)
+numero = 97
+print(numero)
+decimal = 9.7
+print(decimal)
+
+# CONCATENAR VARIABLES
+nombre = "Alejandro"
+apellido = "González"
+web = "algorodev.com"
+print(nombre + ' ' + apellido + ' - ' + web)
+# MEJOR FORMA DE CONCATENAR
+print(f"{nombre} {apellido} - {web}")
+print("Hola me llamo {} {} y mi web es: {}".format(nombre, apellido, web))
+
