3d88895772aaf48d4744549cae82922c8b717162
algoroloro <algoro9697@gmail.com>
2021-01-31 11:16:48 +0100

    Ejercicio 1 en PYTHON

---
+++ PYTHON VANILLA/EJERCICIOS/ejercicio-01.py
@@ -0,0 +1,17 @@
+"""
+ Ejercicio 1
+    - Crear variables: pais y continente
+    - Mostrar su valor por pantalla
+    - Decir el tipo de dato de cada variable
+"""
+
+# MI SOLUCIÓN
+pais = "España"         # string
+continente = "Europa"   # string
+year = 2021             # int
+coronavirus = True      # boolean
+
+print(f"País: {pais}")
+print(f"Continente: {continente}")
+print(f"Año: {year}")
+print(f"Coronavirus: {coronavirus}")
