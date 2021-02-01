4ec2f8c6cfadc2a1584fc1a7a88e5b3e897ac706
algoroloro <algoro9697@gmail.com>
2021-02-01 20:53:28 +0100

    Funciones Curiosidades en PYTHON

---
+++ PYTHON VANILLA/08-FUNCIONES/curiosidades.py
@@ -0,0 +1,14 @@
+def miFuncion(nombre):
+    return f"Hola que tal {nombre}"
+
+def miSegundaFuncion(nombre, apellido):
+    return f"Hola que tal {nombre} {apellido}"
+
+nombre = "Alex"
+apellido = "González"
+
+print("Hola mundo")
+print(f"Bienvenido {nombre}")
+
+print(miFuncion(nombre))
+print(miSegundaFuncion(nombre, apellido))
